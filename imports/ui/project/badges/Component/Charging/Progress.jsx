fbd3bd77ade888b572324f21d7a8ebd76a1111e7
bertrandBecode <bertrand@becode.org>
2018-03-25 19:22:46 +0200

    test

--- imports/ui/project/badges/Component/Charging/Progress.jsx
+++ imports/ui/project/badges/Component/Charging/Progress.jsx
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 
-    const styles = {
-        image: {
-            margin: "auto",
-            width: "50%",
-            display: "block"
-        }
-    }
-
 export default class Progress extends Component {
     render() {
         return (
             <div className="mainProcess">
-                <img src="https://cdn.dribbble.com/users/140957/screenshots/2533654/loading-animation.gif" style={styles.image} alt='logo'/>
                 <div className="loader-wrapper">
                     <div className="loader">
                         <div className="roller"></div>
